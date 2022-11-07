64f6b4c3a6dfe84c69f7157be0bc57f425fce96a
yim3370 <71271289+yim3370@users.noreply.github.com>
2022-11-07 19:13:41 +0900

    #5 spring initailizr로 디렉토리 초기화 완료

---
+++ board-practice-basic/src/main/java/com/board/boardpracticebasic/BoardPracticeBasicApplication.java
@@ -0,0 +1,13 @@
+package com.board.boardpracticebasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardPracticeBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardPracticeBasicApplication.class, args);
+    }
+
+}
---
+++ board-practice-basic/src/test/java/com/board/boardpracticebasic/BoardPracticeBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.boardpracticebasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardPracticeBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
